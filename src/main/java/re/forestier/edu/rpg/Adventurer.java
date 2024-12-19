7d3578fa9eb8f30c2e27843f42b5f4cdecccc829
Your Name <you@example.com>
2024-12-19 10:50:48 +0100

    Add Adventurer, Archer, and Dwarf classes extending Player

---
+++ src/main/java/re/forestier/edu/rpg/Adventurer.java
@@ -0,0 +1,10 @@
+package re.forestier.edu.rpg;
+
+import java.util.ArrayList;
+
+public class Adventurer extends Player {
+    public Adventurer(String playerName, String avatarName, String avatarClass, int money, ArrayList<String> inventory) {
+        super(playerName, avatarName, avatarClass, money, inventory);
+    }
+    
+}
