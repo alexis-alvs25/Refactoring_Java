d55097ce4d33b0e5203e7ee55294da7988ed563c
Your Name <you@example.com>
2024-12-20 15:12:34 +0100

    Removing unused imports in GlobalTest

--- src/test/java/re/forestier/edu/GlobalTest.java
+++ src/test/java/re/forestier/edu/GlobalTest.java
@@ -1,18 +1,11 @@
 package re.forestier.edu;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import re.forestier.edu.rpg.AvatarClass;
 import re.forestier.edu.rpg.Player;
-import re.forestier.edu.rpg.UpdatePlayer;
-import re.forestier.edu.rpg.Player;
-
 import java.util.ArrayList;
 
 import static org.approvaltests.Approvals.verify;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-import static org.junit.jupiter.api.Assertions.fail;
 
 public class GlobalTest {
 
